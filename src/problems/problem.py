7fb185aede733b8b6400dc678f7c620602988623
Benoit Pauwels <benoit.pauwels@irt-saintexupery.com>
2021-04-19 15:40:48 +0200

    feat(problem): add properties

--- src/problems/problem.py
+++ src/problems/problem.py
@@ -28,35 +28,108 @@ class Problem(object):
     ):  # type: (...) -> None
         self._name = name
         self._creator = creator
+
+        # Set the dimension
+        problem = creator()
+        if not isinstance(problem, OptimizationProblem):
+            raise TypeError("Creator must return an OptimizationProblem")
+        self._dimension = problem.dimension
+
+        # Set the starting points
+        for a_point in start_points:
+            if not isinstance(a_point, ndarray):
+                raise TypeError("Starting points must be of type ndarray")
+            elif a_point.shape != (self._dimension,):
+                raise ValueError("Starting points must be 1-dimensional with size {}"
+                                 .format(self._dimension))
         self._start_points = start_points
+
         self._target_values = target_values
+
+    @property
+    def name(self):  # type: (...) -> str
+        """The name of the benchmarking problem."""
+        return self._name
+
+    @property
+    def start_points(self):  # type: (...) -> Iterable[ndarray]
+        """The starting points of the benchmarking problem."""
+        return self._start_points
+
+    @property
+    def target_values(self):  # type: (...) -> TargetValues
+        """The target values of the benchmarking problem."""
+        if self._target_values is None:
+            raise ValueError("Benchmarking problem has no target")
+        return self._target_values
+
+    def __iter__(self):  # type: (...) -> OptimizationProblem
+        """Iterate on the problem instances with respect to the starting points. """
+        for a_start_point in self._start_points:
+            problem = self._creator()
+            problem.design_space.set_current_x(a_start_point)
+            yield problem
+
+    def get_instance(
+            self,
+            start_point=None  # type: Optional[ndarray]
+    ):  # type: (...) -> OptimizationProblem
+        """Return an instance of the benchmarking problem.
+
+        Args:
+            start_point: The starting point of the instance.
+                By default it is the current design of the benchmarking problem.
+        """
+        # TODO: remove this method
+        instance = self._creator()
+        if start_point is not None and start_point in self._start_points:
+            instance.design_space.set_current_x(start_point)
+        return instance
+
+    def is_algorithm_suited(
+            self,
+            name,  # type: str
+    ):  # type: (...) -> bool
+        """Check whether an algorithm is suited to the problem.
+
+        Args:
+            name: The name of the algorithm.
+        """
+        library = OptimizersFactory().create(name)
+        return library.is_algorithm_suited(library.lib_dict[name], self._creator())
 
     def generate_targets(
             self,
             targets_number,  # type: int
             reference_algorithms,  # type: Dict[str, Dict]
+            feasible=True,  # type: bool
     ):  # type: (...) -> TargetValues
         """Generate targets based on reference algorithms.
 
         Args:
             targets_number: The number of targets to generate.
             reference_algorithms: The names and options of the reference algorithms.
+            feasible: Whether to generate only feasible targets.
 
         Returns:
             The generated targets.
 
         """
-        target_generator = TargetsGenerator()
+        targets_generator = TargetsGenerator()
 
         # Generate reference performance histories
         for an_algo_name, an_algo_options in reference_algorithms.items():
-            problem = self._creator()
-            OptimizersFactory().execute(problem, an_algo_name, **an_algo_options)
-            obj_values, measures, feasibility = self._extract_performance(problem)
-            target_generator.add_history(obj_values, measures, feasibility)
+            for an_instance in self:
+                OptimizersFactory().execute(
+                    an_instance, an_algo_name, **an_algo_options
+                )
+                obj_values, measures, feasibility = self.extract_performance(
+                    an_instance
+                )
+                targets_generator.add_history(obj_values, measures, feasibility)
 
         # Compute the target values
-        target_values = target_generator.run(targets_number)
+        target_values = targets_generator.run(targets_number, feasible=feasible)
         self._target_values = target_values
 
         return target_values
@@ -80,17 +153,20 @@ class Problem(object):
 
         # Generate the performance histories
         for an_algo_name, an_algo_options in algorithms.items():
-            problem = self._creator()
-            OptimizersFactory().execute(problem, an_algo_name, **an_algo_options)
-            obj_values, measures, feasibility = self._extract_performance(problem)
-            data_profile.add_history(self._name, an_algo_name, obj_values, measures,
-                                     feasibility)
+            for start_point in self._start_points:
+                problem = self.get_instance(start_point)
+                OptimizersFactory().execute(problem, an_algo_name, **an_algo_options)
+                obj_values, measures, feasibility = self.extract_performance(problem)
+                data_profile.add_history(self._name, an_algo_name, obj_values, measures,
+                                         feasibility)
 
         # Plot and/or save the data profile
         data_profile.plot(show=show, destination_path=destination_path)
 
+    # TODO: remove this method (use ProblemsGroup)
+
     @staticmethod
-    def _extract_performance(
+    def extract_performance(
             problem  # type: OptimizationProblem
     ):  # type: (...) -> Tuple[List[float], List[float], List[bool]]
         """Extract the performance history from a solved optimization problem.
