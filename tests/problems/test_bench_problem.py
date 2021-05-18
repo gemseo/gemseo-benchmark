5806513f3305b264fda926f0df214c345b753569
Benoit Pauwels <benoit.pauwels@irt-saintexupery.com>
2021-05-18 11:33:01 +0200

    fix(test_bench_problem): fix import

--- tests/problems/test_bench_problem.py
+++ tests/problems/test_bench_problem.py
@@ -2,10 +2,10 @@ from gemseo.problems.analytical.rosenbrock import Rosenbrock
 from numpy import zeros
 from pytest import raises
 
-from problems.problem import BenchmarkingProblem
+from problems.problem import Problem
 
 
 def test_invalid_start_points():
     """Check initialization with invalid starting points"""
     with raises(ValueError):
-        BenchmarkingProblem("Rosenbrock2D", Rosenbrock, [zeros(2), zeros(3)])
+        Problem("Rosenbrock2D", Rosenbrock, [zeros(2), zeros(3)])
