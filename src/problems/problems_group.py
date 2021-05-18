b44b22cf6fe1a6e54587a37aaaf2b37229cdcf3e
Benoit Pauwels <benoit.pauwels@irt-saintexupery.com>
2021-05-18 11:30:05 +0200

    docs(problems_group): add header and improve docstrings

--- src/problems/problems_group.py
+++ src/problems/problems_group.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This work is licensed under a BSD 0-Clause License.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+# FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or initial
+#                           documentation
+#        :author: Benoit Pauwels
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
+"""Grouping of reference problems for benchmarking."""
 from pathlib import Path
 from typing import Dict, Iterable, Iterator, List, Optional
 
@@ -7,9 +30,11 @@ from problems.problem import Problem
 
 
 class ProblemsGroup(object):
-    """A group of benchmarking problems."""
+    """A group of reference problems for benchmarking.
 
-    # TODO: explain why problems should be grouped.
+    N.B. reference problems should be grouped based on common characteristics such as
+    functions smoothness and constraint set geometry.
+    """
 
     def __init__(
             self,
@@ -23,7 +48,6 @@ class ProblemsGroup(object):
             problems: The benchmarking problems of the group.
             description: The description of the group of problems.
         """
-        # TODO: check that every problem has the same number of starting points
         self._name = name
         self._problems = problems
         self._description = description
