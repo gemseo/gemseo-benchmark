f66e4a11a3dd1d85dd8ee6ed6c3331e7fd186bf9
Benoit Pauwels <benoit.pauwels@irt-saintexupery.com>
2021-09-30 15:15:34 +0200

    fix(performance_history): handle absence of evaluations budget

--- src/gemseo_benchmark/results/performance_history.py
+++ src/gemseo_benchmark/results/performance_history.py
@@ -293,7 +293,7 @@ class PerformanceHistory(Sequence[HistoryItem]):
         if self.algorithm is None:
             raise ValueError("The algorithm name is not set.")
         cumulated_minimum = self.compute_cumulated_minimum()
-        if len(cumulated_minimum) < self.max_eval:
+        if self.max_eval is not None and len(cumulated_minimum) < self.max_eval:
             # Extend the history up to the evaluations budget
             cumulated_minimum.history_items.extend(
                 [cumulated_minimum[-1]] * (self.max_eval - len(cumulated_minimum))
