d39b27bfd2fda9bfd7e50f142a796e2d34631136
Benoît Pauwels <benoit.pauwels@irt-saintexupery.com>
2022-06-28 15:13:36 +0200

    feat(runner): add a benchmarking runner

---
+++ src/gemseo_benchmark/runner.py
@@ -0,0 +1,416 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or initial
+#                           documentation
+#        :author: Benoit Pauwels
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
+"""A benchmarking runner to run optimization algorithms on reference problems."""
+import time
+from pathlib import Path
+from typing import Iterable, Tuple
+
+from gemseo.algos.database import Database
+from gemseo.algos.opt.opt_factory import OptimizersFactory
+from gemseo.algos.opt_problem import OptimizationProblem
+from gemseo.api import configure_logger, execute_algo
+from gemseo_benchmark import join_substrings
+from gemseo_benchmark.algorithms.algorithm_configuration import AlgorithmConfiguration
+from gemseo_benchmark.algorithms.algorithms_configurations import \
+    AlgorithmsConfigurations
+from gemseo_benchmark.problems.problem import Problem
+from gemseo_benchmark.results.performance_history import PerformanceHistory
+from gemseo_benchmark.results.results import Results
+
+LOGGER = configure_logger()
+
+
+class Runner(object):
+    """A benchmarking runner to run optimization algorithms on reference problems."""
+    _HISTORY_CLASS = PerformanceHistory
+
+    def __init__(
+            self,
+            histories_dir: Path,
+            results_file: Path,
+            databases_dir: Path = None,
+            pseven_dir: Path = None
+    ) -> None:
+        """
+        Args:
+            histories_dir: The path to the directory where to save the performance
+                histories.
+            results_file: The path to the file for loading/saving the performance
+                histories paths.
+            databases_dir: The path to the destination directory for the databases.
+            pseven_dir: The path to the destination directory for the pSeven output
+                files.
+        """
+        self._databases_dir = databases_dir
+        self.__histories_dir = histories_dir
+        self.__pseven_dir = pseven_dir
+        self.__results_file = results_file
+        if results_file.is_file():
+            self._results = Results(results_file)
+        else:
+            self._results = Results()
+
+    def execute(
+            self,
+            problems: Iterable[Problem],
+            algorithms: AlgorithmsConfigurations,
+            overwrite_histories: bool = False
+    ) -> Results:
+        """Run optimization algorithms on reference problems.
+
+        Args:
+            problems: The benchmarking problems.
+            algorithms: The algorithms configurations.
+            overwrite_histories: Whether to overwrite existing performance histories.
+
+        Returns:
+            The results of the optimization.
+
+        Raises:
+            ValueError: If the algorithm is not available.
+        """
+        for algorithm_configuration in algorithms:
+
+            # Check that the algorithm is available
+            algorithm_name = algorithm_configuration.algorithm_name
+            if not OptimizersFactory().is_available(
+                    algorithm_configuration.algorithm_name
+            ):
+                raise ValueError(
+                    f"The algorithm is not available: {algorithm_name}."
+                )
+
+            algo_config = self.__disable_stopping_criteria(algorithm_configuration)
+            for bench_problem in problems:
+                self._solve_problem(
+                    bench_problem, algo_config, overwrite_histories=overwrite_histories
+                )
+
+        return self._results
+
+    @staticmethod
+    def __disable_stopping_criteria(
+            algorithm_configuration: AlgorithmConfiguration
+    ) -> AlgorithmConfiguration:
+        """Disable the stopping criteria.
+
+        Args:
+            algorithm_configuration: The algorithm configuration.
+
+        Returns:
+            A copy of the algorithm configuration with disabled stopping criteria.
+        """
+        options = {
+            "xtol_rel": 0.0,
+            "xtol_abs": 0.0,
+            "ftol_rel": 0.0,
+            "ftol_abs": 0.0,
+            "stop_crit_n_x": int(1e10),  # value larger than the database size
+        }
+        options.update(algorithm_configuration.algorithm_options)
+        return AlgorithmConfiguration(
+            algorithm_configuration.algorithm_name,
+            algorithm_configuration.name,
+            **options
+        )
+
+    def _solve_problem(
+            self,
+            problem: Problem,
+            algorithm_configuration: AlgorithmConfiguration,
+            overwrite_histories: bool
+    ) -> None:
+        """Run an algorithm on a benchmarking problem for all its starting points.
+
+        Args:
+            problem: The benchmarking problem.
+            algorithm_configuration: The algorithm configuration.
+            overwrite_histories: Whether to overwrite existing performance histories.
+        """
+        # Run an optimization from each starting point
+        for index, instance in enumerate(problem):
+
+            # Check whether the instance should be skipped
+            if self.__skip_instance(
+                    algorithm_configuration, problem, index, overwrite_histories
+            ):
+                continue
+
+            # Run the optimization
+            algo_config = self.__set_pseven_log_file(
+                algorithm_configuration, problem, index
+            )
+            database, history = self._run_algorithm(
+                instance, algo_config, problem.name, index
+            )
+
+            # Save the performance history
+            self._save_history(history, algorithm_configuration, index)
+
+            # Save the database
+            self.__save_database(
+                database, algorithm_configuration, problem.name, index
+            )
+
+            # Update the results file
+            self._results.to_file(self.__results_file, indent=4)
+
+    def __skip_instance(
+            self,
+            algorithm_configuration: AlgorithmConfiguration,
+            bench_problem: Problem,
+            index: int,
+            overwrite_histories: bool
+    ) -> bool:
+        """Check whether a problem instance has already been solved.
+
+        Args:
+            algorithm_configuration: The algorithm configuration.
+            bench_problem: The benchmarking problem.
+            index: The index of the instance.
+            overwrite_histories: Whether to overwrite existing histories.
+
+        Returns:
+            Whether to solve the problem instance.
+        """
+        instance = index + 1
+        problem_name = bench_problem.name
+
+        # Check whether the result is already stored
+        path = self.__get_history_path(algorithm_configuration, problem_name, index)
+        if not overwrite_histories and self._results.contains(
+                algorithm_configuration.name, problem_name, path
+        ):
+            LOGGER.info(
+                f"Skipping instance {instance} of problem {problem_name} for algorithm "
+                f"configuration {algorithm_configuration.name}."
+            )
+            return True
+
+        LOGGER.info(
+            f"Solving instance {instance} of problem {problem_name} with algorithm "
+            f"configuration {algorithm_configuration.name}."
+        )
+        return False
+
+    def __set_pseven_log_file(
+            self,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem: Problem,
+            index: int
+    ) -> AlgorithmConfiguration:
+        """Return a copy of a configuration including the path to the pSeven log file.
+
+        Args:
+            algorithm_configuration: The algorithm configuration.
+            problem: The benchmarking problem.
+            index: The index of the problem instance.
+
+        Returns:
+            A copy of the configuration including the path to the pSeven log file.
+        """
+        if self.__pseven_dir is None or not OptimizersFactory().is_available("PSEVEN"):
+            return algorithm_configuration
+
+        from gemseo.algos.opt.lib_pseven import PSevenOpt
+        if algorithm_configuration.algorithm_name not in PSevenOpt().descriptions:
+            return algorithm_configuration
+
+        log_path = self.__get_pseven_log_path(
+            algorithm_configuration, problem.name, index
+        )
+        return AlgorithmConfiguration(
+            algorithm_configuration.algorithm_name,
+            algorithm_configuration.name,
+            **algorithm_configuration.algorithm_options, log_path=str(log_path)
+        )
+
+    def _run_algorithm(
+            self,
+            problem: OptimizationProblem,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem_name: str,
+            index: int
+    ) -> Tuple[Database, PerformanceHistory]:
+        """Run an algorithm on a benchmarking problem for a particular starting point.
+
+        Args:
+            problem: The instance of the benchmarking problem.
+            algorithm_configuration: The algorithm configuration.
+            problem_name: The name of the benchmarking problem.
+            index: The index of the problem instance.
+
+        Returns:
+            The runtime of the solver.
+        """
+        algo_name = algorithm_configuration.algorithm_name
+        algo_options = algorithm_configuration.algorithm_options
+
+        # Run the algorithm on the optimization problem.
+        start_time = time.time()
+        execute_algo(problem, algo_name, **algo_options)
+        runtime = time.time() - start_time
+
+        history = self._HISTORY_CLASS.from_problem(problem, problem_name)
+        history.algorithm_configuration = algorithm_configuration
+
+        # Set the DOE size
+        if OptimizersFactory().is_available("PSEVEN"):
+            from gemseo.algos.opt.lib_pseven import PSevenOpt
+            if algo_name in PSevenOpt().descriptions and "sample_x" in algo_options:
+                history.doe_size = len(algo_options["sample_x"])
+            else:
+                # The DOE is made of the single starting point
+                history.doe_size = 1
+        else:
+            history.doe_size = 1
+
+        history.total_time = runtime
+        return problem.database, history
+
+    def _save_history(
+            self,
+            history: PerformanceHistory,
+            algorithm_configuration: AlgorithmConfiguration,
+            index: int
+    ) -> None:
+        # TODO: pass the algorithm configuration through the history
+        """Save a performance history into a history file.
+
+        Args:
+            history: The performance history.
+            algorithm_configuration: The algorithm configuration.
+            index: The index of the problem instance.
+        """
+        problem_name = history.problem_name
+        path = self.__get_history_path(
+            algorithm_configuration, problem_name, index, make_parents=True
+        )
+        history.to_file(path)
+        self._results.add_path(algorithm_configuration.name, problem_name, path)
+
+    def __get_history_path(
+            self,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem_name: str,
+            index: int,
+            make_parents: bool = False
+    ) -> Path:
+        """ Return a path for a history file.
+
+        Args:
+            algorithm_configuration: The algorithm configuration.
+            problem_name: The name of the problem.
+            index: The index of the problem instance.
+            make_parents: Whether to make the parent directories.
+
+        Returns:
+            The path for the history file.
+        """
+        return self._get_path(
+            self.__histories_dir, algorithm_configuration, problem_name, index, "json",
+            make_parents=make_parents
+        )
+
+    def __get_pseven_log_path(
+            self,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem_name: str,
+            index: int
+    ) -> Path:
+        """Return a path for a pSeven log file.
+
+        Args:
+            algorithm_configuration: The algorithm configuration.
+            problem_name: The name of the problem.
+            index: The index of the problem instance.
+
+        Returns:
+            The path for the pSeven log file.
+
+        Raises:
+            ValueError: If the path to the destination directory for the
+                pSeven files is not set.
+        """
+        if self.__pseven_dir is None:
+            raise ValueError("The directory for the pSeven files is not set.")
+
+        return self._get_path(
+            self.__pseven_dir, algorithm_configuration, problem_name, index, "txt",
+            make_parents=True
+        )
+
+    @staticmethod
+    def _get_path(
+            root_dir: Path,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem_name: str,
+            index: int,
+            extension: str = "json",
+            make_parents: bool = False
+    ) -> Path:
+        """Return a path in the file tree dedicated to a specific optimization run.
+
+        Args:
+            root_dir: The path to the root directory.
+            algorithm_configuration: The algorithm configuration.
+            problem_name: The name of the problem.
+            index: The index of the problem instance.
+            extension: The extension of the path.
+                If None, the extension is for a JSON file.
+            make_parents: Whether to make the parent directories
+
+        Returns:
+            The path for the file.
+        """
+        configuration_name = join_substrings(algorithm_configuration.name)
+        path = root_dir.resolve() / configuration_name / join_substrings(
+            problem_name
+        ) / f"{configuration_name}.{index + 1}.{extension}"
+        if make_parents:
+            path.parent.mkdir(parents=True, exist_ok=True)
+
+        return path
+
+    def __save_database(
+            self,
+            database: Database,
+            algorithm_configuration: AlgorithmConfiguration,
+            problem_name: str,
+            index: int
+    ) -> None:
+        """Save the database of a problem.
+
+        Args:
+            database: The database.
+            algorithm_configuration: The algorithm configuration.
+            problem_name: The name of the problem.
+            index: The index of the problem instance.
+        """
+        if self._databases_dir is None:
+            return
+
+        path = self._get_path(
+            self._databases_dir, algorithm_configuration, problem_name, index, "h5",
+            make_parents=True
+        )
+        database.export_hdf(path)
---
+++ tests/test_runner.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or initial
+#                           documentation
+#        :author: Benoit Pauwels
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
+"""Tests for the benchmarking runner."""
+
+import pytest
+from numpy import array
+
+from gemseo.problems.analytical.rastrigin import Rastrigin
+from gemseo.problems.analytical.rosenbrock import Rosenbrock
+from gemseo_benchmark.algorithms.algorithm_configuration import AlgorithmConfiguration
+from gemseo_benchmark.algorithms.algorithms_configurations import \
+    AlgorithmsConfigurations
+from gemseo_benchmark.problems.problem import Problem
+from gemseo_benchmark.runner import Runner
+
+
+@pytest.fixture(scope="module")
+def rosenbrock() -> Problem:
+    """A benchmarking problem based on the 2-dimensional Rosenbrock function."""
+    return Problem("Rosenbrock", Rosenbrock, [array([0.0, 1.0]), array([1.0, 0.0])])
+
+
+@pytest.fixture(scope="module")
+def rastrigin() -> Problem:
+    """A benchmarking problem based on the 2-dimensional Rastrigin function."""
+    return Problem("Rastrigin", Rastrigin, [array([0.0, 0.1]), array([0.1, 0.0])])
+
+
+def test__save_history(tmp_path, rosenbrock):
+    """Check the saving of performance histories."""
+    results_path = tmp_path / "results.json"
+    algo_config = AlgorithmConfiguration("L-BFGS-B")
+    results = Runner(tmp_path, results_path).execute(
+        [rosenbrock], AlgorithmsConfigurations(algo_config)
+    )
+    algo_pb_dir = tmp_path / algo_config.name / rosenbrock.name
+    path = algo_pb_dir / f"{algo_config.name}.1.json"
+    assert path.is_file()
+    assert results.contains(algo_config.algorithm_name, rosenbrock.name, path)
+    assert f"Solving instance 1 of problem {rosenbrock.name} with algorithm " \
+           f"configuration {algo_config.name}."
+    path = algo_pb_dir / f"{algo_config.name}.2.json"
+    assert path.is_file()
+    assert results.contains(algo_config.algorithm_name, rosenbrock.name, path)
+    assert f"Solving instance 2 of problem {rosenbrock.name} with algorithm " \
+           f"configuration {algo_config.name}."
+
+
+def test___save_database(tmp_path, rosenbrock):
+    """Check the saving of optimization databases."""
+    algo_config = AlgorithmConfiguration("L-BFGS-B")
+    Runner(tmp_path, tmp_path / "results.json", tmp_path).execute(
+        [rosenbrock], AlgorithmsConfigurations(algo_config)
+    )
+    algo_pb_dir = tmp_path / algo_config.name / rosenbrock.name
+    assert (algo_pb_dir / f"{algo_config.name}.1.h5").is_file()
+    assert (algo_pb_dir / f"{algo_config.name}.2.h5").is_file()
+
+
+def test_unavailable_algorithm(
+        tmp_path, rosenbrock, unknown_algorithm_configuration,
+        unknown_algorithms_configurations
+):
+    """Check the handling of an unavailable algorithm."""
+    with pytest.raises(
+            ValueError, match="The algorithm is not available: "
+                              f"{unknown_algorithm_configuration.algorithm_name}."
+    ):
+        Runner(tmp_path, tmp_path / "results.json").execute(
+            [rosenbrock], unknown_algorithms_configurations
+        )
+
+
+def test___skip_instance(tmp_path, rosenbrock, rastrigin, caplog):
+    """Check the skipping of an optimization."""
+    results_path = tmp_path / "results.json"
+    algo_config = AlgorithmConfiguration("L-BFGS-B")
+    # Run the algorithm on the Rosenbrock problem
+    Runner(tmp_path, results_path).execute(
+        [rosenbrock], AlgorithmsConfigurations(algo_config)
+    )
+    # Run the algorithm on the Rastrigin problem
+    Runner(tmp_path, results_path).execute(
+        [rosenbrock, rastrigin], AlgorithmsConfigurations(algo_config)
+    )
+    assert f"Skipping instance 1 of problem {rosenbrock.name} for algorithm " \
+           f"configuration {algo_config.name}." in caplog.text
+    assert f"Skipping instance 2 of problem {rosenbrock.name} for algorithm " \
+           f"configuration {algo_config.name}." in caplog.text
