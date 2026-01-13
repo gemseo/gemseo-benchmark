087501955528b0f134ccd44fe59b3b97393c4847
CI <group_11547342_bot_857959eb1dea5ce3d5541af3d12d09a3@noreply.gitlab.com>
2026-01-13 16:29:41 +0000

    Update dependencies

--- tests/problems/test_mdo_problem_configuration.py
+++ tests/problems/test_mdo_problem_configuration.py
@@ -14,6 +14,8 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Tests for MDO problem configurations."""
 
+from __future__ import annotations
+
 from unittest import mock
 
 from matplotlib.testing.decorators import image_comparison
--- tests/report/test_axis_data.py
+++ tests/report/test_axis_data.py
@@ -14,6 +14,8 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Tests of getting data for a plot axis."""
 
+from __future__ import annotations
+
 import matplotlib
 import matplotlib.pyplot
 import pytest
