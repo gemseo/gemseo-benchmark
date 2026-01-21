58707be1ace2ee3e4bd682b64533e506e030ca24
Charles Thoulon <charles.thoulon@gmail.com>
2026-01-21 14:39:59 +0000

    Reactivate logger

--- tests/conftest.py
+++ tests/conftest.py
@@ -263,7 +263,10 @@ def algorithm_configuration() -> mock.Mock:
     """The configuration of an algorithm."""
     algo_config = mock.Mock()
     algo_config.algorithm_name = "SLSQP"
-    algo_config.algorithm_options = {"normalize_design_space": False, "max_iter": 3}
+    algo_config.algorithm_options = {
+        "normalize_design_space": False,
+        "max_iter": 3,
+    }
     algo_config.name = "SLSQP"
     algo_config.instance_algorithm_options = {}
     algo_config.copy = mock.Mock(return_value=algo_config)
